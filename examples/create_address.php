92044a62ccb12a277c73e0faeadf169929eda900
xpayapi <xpayapi@gmail.com>
2024-03-04 07:04:25 +0700

    Initial Build

---
+++ examples/create_address.php
@@ -0,0 +1,105 @@
+<?php
+
+    require_once __DIR__ . "/../src/xpayapi_sci.class.php";
+
+    $secret_keys_and_config = [
+        "merchant_id" => "Merchant ID",
+        "merchant_password" => "Merchant Password",
+        "api_id" => "API ID",
+        "api_password" => "API Password",
+        "config" => [
+            "test_mode" => false,
+        ],
+    ];
+
+    include_once __DIR__ . "/../config/config-example.php";
+
+    /*
+     * BitCoin: [ BTC ],
+     * Ethereum: [ ETH ],
+     * LiteCoin: [ LTC ],
+     * DogeCoin: [ DOGE ],
+     * Dash: [ DASH ],
+     * BitcoinCash: [ BCH ],
+     * Zcash: [ ZEC ],
+     * EthereumClassic: [ ETC ],
+     * Ripple: [ XRP ],
+     * TRON: [ TRX ],
+     * Stellar: [ XLM ],
+     * BinanceCoin: [ BNB ],
+     * TRON_TRC20: [ USDT ],
+     * BinanceSmartChain_BEP20: [ USDT, BUSD, USDC, ADA, EOS, BTC, ETH, DOGE, SHIB ],
+     * Ethereum_ERC20: [ USDT, BUSD, USDC, SHIB ],
+     */
+    $xPayApi_params = [
+        "amount" => null,
+        "system" => "TRON_TRC20",
+        "currency" => "USDT",
+        "order_id" => "New order id "  . microtime(true),
+        "comment" => "Testing comment",
+    ];
+
+    $xPayApi = new \xPayApi\xPayApiSCI(
+        $secret_keys_and_config["merchant_id"],
+        $secret_keys_and_config["merchant_password"],
+        $secret_keys_and_config["config"]["test_mode"]
+    );
+
+    $res = $xPayApi->createAddress(
+        $xPayApi_params["amount"],
+        $xPayApi_params["system"],
+        $xPayApi_params["currency"],
+        $xPayApi_params["order_id"],
+        $xPayApi_params["comment"],
+        $xPayApi_params["address_callback"],
+        $xPayApi_params["custom"]
+    );
+
+
+    if ($res['error']) {
+        echo $res['message'];
+        // more actions in case of an error, update database, send message or etc
+    } else {
+        // if you want to redirect your user to our payments gateway use this "pay_link"
+        $pay_link = $res["data"]["pay_link"];
+
+        // other wise use a data that given in callback
+        $invoice_id = $res['data']['invoice'];
+        $address = $res["data"]["address"];
+        $tag = $res["data"]["tag"];
+        $tag_name = $res["data"]["tag_name"];
+        $is_tag = $res["data"]["is_tag"];
+
+        $system = $res["data"]["system"];
+        $currency = $res["data"]["currency"];
+
+        $display = sprintf("address %s", $address);
+        if ($is_tag) {
+            $display = sprintf("address %s %s: %s", $address, mb_convert_case($tag_name, MB_CASE_TITLE), $tag);
+        }
+
+        if (null === $xPayApi_params["amount"]) {
+            echo sprintf(
+                "Send a money to the %s %s.",
+                $system,
+                htmlspecialchars($display,ENT_QUOTES, "UTF-8")
+            );
+        } else {
+            echo sprintf(
+                "Send %s %s to the %s %s.",
+                $xPayApi_params["amount"],
+                $currency,
+                $system,
+                htmlspecialchars($display, ENT_QUOTES, "UTF-8")
+            );
+        }
+
+        //Creating QR
+        $qr_request = $xPayApi->getQrLink($res['data'], $xPayApi_params["amount"]);
+        if (!$qr_request["error"]) {
+            echo sprintf(
+                '<br><br>QR Code:<br><img alt="" src="http://chart.apis.google.com/chart?cht=qr&chs=300x300&chl=%s">',
+                $qr_request["data"]["link"]
+            );
+        }
+    }
---
+++ templates/README.md.php
@@ -0,0 +1,128 @@
+# xPayapi.com PHP SCI/API Wrapper(Official).
+
+This is an official module developed by xPayapi.com for easy integration into PHP-based applications.
+Source code of the class files is in the ./src directory. The source code for the examples can be found at ./examples.
+
+
+## Requirements
+It's recommended to use a newer version of PHP. This library was written in a PHP v7.1+ environment + php-curl, php-json, php-mbstring modules.
+
+A xPayapi.com account with **Merchant ID, Merchant Password, API ID, API Password**. You can get the credentials at the pages: [Add merchant](https://xPayapi.com/merchant/add/), [Add API](https://xPayapi.com/api/add/).
+
+## Installation
+
+Package available on [Composer](https://packagist.org/packages/xpayapi/xpayapi_com).
+
+If you're using Composer to manage dependencies, you can use
+
+```bash
+$ composer require xpayapi/xpayapi_com
+```
+
+## Test examples with Docker
+
+To quickly run examples you need to install Docker, git and make utility.
+
+**Step 1:**
+
+```bash
+$ git clone https://github.com/xpayapi/xpayapi_com-modules.git
+$ cd xpayapi_com-modules/
+```
+
+**Step 2:**
+
+Setup config with credentials *config/config-example.php*
+
+**Step 3:**
+
+```bash
+$ make run
+```
+
+**Step 4:**
+
+Open examples in your browser *examples/*
+
+
+**Example:**
+
+Follow the link [http://localhost/examples/custom_payment_page.php](http://localhost/examples/custom_payment_page.php)
+
+**Stop server:**
+
+```bash
+$ make stop
+```
+
+## Usage
+
+<?php
+    $examples = [
+        [
+                "file" => "custom_payment_page.php",
+                "title" => "Custom payment page",
+        ],
+        [
+            "file" => "create_address.php",
+            "title" => "Get a payment address and a QR-code for him",
+        ],
+        [
+            "file" => "processing_ipn_for_transaction.php",
+            "title" => "Check an IPN of a transaction",
+        ],
+        [
+            "file" => "create_payment_link.php",
+            "title" => "Get a payment link(create an order)",
+        ],
+        [
+            "file" => "processing_ipn_for_order.php",
+            "title" => "Check an IPN of an order",
+        ],
+        [
+            "file" => "send_money.php",
+            "title" => "Send a money",
+        ],
+        [
+            "file" => "get_merchant_balance.php",
+            "title" => "Get a merchant balance",
+        ],
+        [
+            "file" => "get_history.php",
+            "title" => "Get a merchant history",
+        ],
+        [
+            "file" => "get_merchant_info.php",
+            "title" => "Get a merchant info",
+        ],
+        [
+            "file" => "get_currency_rates.php",
+            "title" => "Get cryptocurrency pair rates",
+        ],
+        [
+            "file" => "get_available_currencies.php",
+            "title" => "Get available currencies",
+        ],
+    ];
+
+?>
+
+<?php foreach ($examples as $item) { ?>
+### <?php echo $item["title"]; ?>
+
+```php
+<?php echo file_get_contents(sprintf("%s%s%s", __DIR__, "/../examples/", $item["file"])); ?>
+
+```
+
+**Example:**
+<?php $link = sprintf("%s%s", "http://localhost/examples/", $item["file"]); ?>
+Follow the link [<?php echo $link; ?>](<?php echo $link; ?>)
+
+<?php } ?>
+
+## Contributing
+If during your work with this wrapper you encounter a bug or have a suggestion to help improve it for others, you are welcome to open a Github issue on this repository and it will be reviewed by one of our development team members. The xPayapi.com bug bounty does not cover this wrapper.
+
+## License
+MIT - see LICENSE
